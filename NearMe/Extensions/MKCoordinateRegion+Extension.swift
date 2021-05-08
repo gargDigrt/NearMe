ae5abf34c7419430b29d9359faf1514bd98ef104
VIvek <gargvivek321@gmail.com>
2021-05-08 23:35:47 +0530

    Initial commit Project created

---
+++ NearMe/Extensions/MKCoordinateRegion+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  MKCoordinateRegion+Extension.swift
+//  NearMe
+//
+//  Created by Vivek on 08/05/21.
+//
+
+import Foundation
---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Vivek on 08/05/21.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
